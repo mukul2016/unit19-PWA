26c7bcaacd6b29698d2ca124a87091a55d372612
Mukul Sharma <msharma@3222-mbp15.attlocal.net>
2020-10-25 19:14:27 -0700

    Adding service-worker.js and other misc changes

---
+++ public/js/idb.js
@@ -0,0 +1,74 @@
+// create variable to hold db connection
+let db;
+// establish a connection to IndexedDB database called 'BW' and set it to version 1
+const request = indexedDB.open('BW', 1);
+
+// this event will emit if the database version changes (nonexistant to version 1, v1 to v2, etc.)
+request.onupgradeneeded = function(event) {
+    // save a reference to the database 
+    const db = event.target.result;
+    // create an object store (table) called `new_pizza`, set it to have an auto incrementing primary key of sorts 
+    db.createObjectStore('new_BWStore', { autoIncrement: true });
+  };
+
+  // upon a successful 
+request.onsuccess = function(event) {
+    // when db is successfully created with its object store (from onupgradedneeded event above) or simply established a connection, save reference to db in global variable
+    db = event.target.result;
+  
+    // check if app is online, if yes run uploadBWData() function to send all local db data to api
+    if (navigator.onLine) {
+      uploadBWData();
+    }
+  };
+  
+  request.onerror = function(event) {
+    // log error here
+    console.log(event.target.errorCode);
+  };
+
+  // This function will be executed if there's no internet connection
+function saveRecord(record) {
+    // open a new transaction with the database with read and write permissions 
+    const transaction = db.transaction(['new_BWStore'], 'readwrite');
+  
+    // access the object store for `new_BWStore`
+    const BWObjectStore = transaction.objectStore('new_BWStore');
+  
+    // add record to your store with add method
+    BWObjectStore.add(record);
+  }
+
+  // upon a successful .getAll() execution, run this function
+getAll.onsuccess = function() {
+    // if there was data in indexedDb's store, let's send it to the api server
+    if (getAll.result.length > 0) {
+      fetch('/api/transaction/bulk', {
+        method: 'POST',
+        body: JSON.stringify(getAll.result),
+        headers: {
+          Accept: 'application/json, text/plain, */*',
+          'Content-Type': 'application/json'
+        }
+      })
+        .then(response => response.json())
+        .then(serverResponse => {
+          if (serverResponse.message) {
+            throw new Error(serverResponse);
+          }
+          // open one more transaction
+          const transaction = db.transaction(['new_BWStore'], 'readwrite');
+          // access the new_pizza object store
+          const BWObjectStore = transaction.objectStore('new_BWStore');
+          // clear all items in your store
+          BWObjectStore.clear();
+
+          alert('All saved transactions has been submitted!');
+        })
+        .catch(err => {
+          console.log(err);
+        });
+    }
+  };
+  // listen for app coming back online
+window.addEventListener('online', uploadBWData);
